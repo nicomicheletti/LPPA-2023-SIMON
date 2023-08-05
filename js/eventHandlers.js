a0559b5cdd99ab9751fc0a62603af39b03210d0e
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2023-08-05 19:42:12 -0300

    functional homepage buttons

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -3,10 +3,26 @@
 var startBtn;
 var rankingBtn;
 
+var redBtn;
+var yellowBtn;
+var blueBtn;
+var greenBtn;
+
 var init = function() {
     startBtn = document.getElementById('startBtn');
     rankingBtn = document.getElementById('rankingBtn');
 
+    redBtn = document.getElementById('redBtn');
+    yellowBtn = document.getElementById('yellowBtn');
+    blueBtn = document.getElementById('blueBtn');
+    greenBtn = document.getElementById('greenBtn');
+
     startBtn.addEventListener('click', handleStartBtn);
-    console.log('newells');
+    rankingBtn.addEventListener('click', handleRankingBtn);
+
+    redBtn.addEventListener('click', handleRedBtn);
+    yellowBtn.addEventListener('click', handleYellowBtn);
+    blueBtn.addEventListener('click', handleBlueBtn);
+    greenBtn.addEventListener('click', handleGreenBtn);
+
 };
--- js/simon.js
+++ js/simon.js
@@ -3,3 +3,23 @@
 var handleStartBtn = function() {
     console.log("click btn");
 };
+
+var handleRankingBtn = function() {
+    console.log("ranking btn")
+}
+
+var handleRedBtn = function() {
+    console.log('click en handleRedBtn');
+};
+
+var handleYellowBtn = function() {
+    console.log('click en handleYellowBtn');
+};
+
+var handleBlueBtn = function() {
+    console.log('click en handleBlueBtn');
+};
+
+var handleGreenBtn = function() {
+    console.log('click en handleGreenBtn');
+};
