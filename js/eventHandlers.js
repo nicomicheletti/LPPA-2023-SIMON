09f616ba00dbb03efa4866f5e1fcf8a1f8319a84
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2023-08-05 19:30:31 -0300

    first variables and buttons test

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,2 +1,12 @@
 'use strict';
 
+var startBtn;
+var rankingBtn;
+
+var init = function() {
+    startBtn = document.getElementById('startBtn');
+    rankingBtn = document.getElementById('rankingBtn');
+
+    startBtn.addEventListener('click', handleStartBtn);
+    console.log('newells');
+};
