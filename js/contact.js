f74dee1bbb128b34b2f91eb3ae5f05c83ba41211
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2023-08-06 13:06:19 -0300

    name and emails fields logic

--- js/contact.js
+++ js/contact.js
@@ -27,4 +27,68 @@ window.onload = function(){
     message.addEventListener('focus', function() {
         reset(message);
     });
-}
+
+    function checkInput (input) {
+        if (input.value === '') {
+            showError(input,'Campos incompletos');
+            return true;
+        };
+    };
+
+    function corroboration (string) {
+        string = string.split("").join("");
+        var control = 0;
+        for (var i = 0; i < string.length; i++){
+            if (Number(string[i]) == string[i]) {
+                control ++;
+            };
+        };
+        if (control == 0) {
+            return false;
+        } else {
+            return true;
+        };
+    };
+
+    function symbolCheck (string) {
+        var symbols = '!"#$%&/()=?¡¿|¨*][_:;,.-{}+¬°~^`@'+"'";
+        var control = 0;
+        for (var i=0; i < string.length; i++) {
+            for(var x=0;x < symbols.length;x++) {
+                if (string[i] == symbols[x]) {
+                    control ++;
+                };
+            };
+        };
+        if (control == 0) {
+            return false;
+        } else {
+            return true;
+        };
+    };
+
+    function checkName (){
+        if (checkInput(name)) {
+            return 'Campo del nombre incompleto';
+        } else if (name.value.length < 3) {
+            showError(name,'Debe contener al menos 3 caracteres');
+            return 'Nombre muy corto';
+        } else if (corroboration(name.value) || symbolCheck(name.value)) {
+            showError(name,'Ingresá un formato válido. No debe tener números ni símbolos');
+            return 'Formato inválido';
+        } else {
+            return '';
+        }
+    };
+
+    function checkEmail () {
+        if (checkInput(email)) {
+            return 'Campo del correo incompleto';
+        } else if (!emailFormat.test(email.value)){
+            showError(email,'Por favor, ingresá un mail válido');
+            return 'Formato inválido';
+        } else {
+            return '';
+        };
+    };
+};
